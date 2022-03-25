295b446861d63a8e030926c37218162bc6b70581
Alexandra <mwalexandra@users.github.com>
2022-03-25 11:49:11 +0100

    small fixes

--- FE/22.03.21_JS_intro/app.js
+++ FE/22.03.21_JS_intro/app.js
@@ -3,13 +3,14 @@
 // For example:
 // The original number is 32452. Digits are '3', '2', '4', '5', '2'. Sum of digits will be 3+2+4+5+2 = 16
 
-let number = 32452;
+let number = 1032;
 let res = 0;
 
-while (number > 1) {
+while (number > 0) {
   res += number % 10;
+  console.log(res);
   number = Math.floor(number / 10);
 }
 
-console.log(res);
+
 
