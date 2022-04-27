d6f220f8f984c2e0b5e9ae72baea0a567a638d80
Alexandra <mwalexandra@users.github.com>
2022-04-27 10:07:54 +0200

    fixes

--- FE/22.04.25_person_book/app.js
+++ FE/22.04.25_person_book/app.js
@@ -2,8 +2,12 @@
 
 const openModalEl = document.querySelector('.open-modal-btn');
 const modal = document.querySelector('.form-modal-wrapper');
+const modalCloseBtn = document.querySelector('.form-modal-form span')
 
 openModalEl.addEventListener('click', () => {
   modal.style.display = 'flex';
 });
 
+modalCloseBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
