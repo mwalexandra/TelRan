5ee0c8aab4460effea878ab4859b8d43189f48fc
Alexandra <mwalexandra@users.github.com>
2022-05-02 12:40:04 +0200

    person book finished

--- FE/22.04.25_person_book/app.js
+++ FE/22.04.25_person_book/app.js
@@ -1,25 +1,36 @@
 const personsList = document.querySelector('.persons-list');
 let personsArray = [
-  {
-    name: 'Alexandra',
-    surname: 'Makeewa',
-    tel: '0123456',
-    email: 'sasha@mail.com',
-    photoLink: 'https://img.com/sasha',
-  },
-  {
-    name: 'John',
-    surname: 'Doe',
-    tel: '6542980',
-    email: 'john@mail.com',
-    photoLink: 'https://img.com/john',
-  },
+  // {
+  //   id: 1,
+  //   name: 'Sasha',
+  //   surname: 'Makeewa',
+  //   tel: '123456',
+  //   email: 'sasha@gmail.com',
+  //   photoLink: 'jfgkfjg.com',
+  // },
+  // {
+  //   id: 2,
+  //   name: 'Sasha2',
+  //   surname: 'Makeewa',
+  //   tel: '123456',
+  //   email: 'sasha@gmail.com',
+  //   photoLink: 'jfgkfjg.com',
+  // },
+  // {
+  //   id: 3,
+  //   name: 'Sasha3',
+  //   surname: 'Makeewa',
+  //   tel: '123456',
+  //   email: 'sasha@gmail.com',
+  //   photoLink: 'jfgkfjg.com',
+  // },
 ];
 
 const openModalEl = document.querySelector('.open-modal-btn');
 const modal = document.querySelector('.form-modal-wrapper');
 const form = document.querySelector('.form-modal-form');
-const modalCloseBtn = document.querySelector('.form-modal-form span')
+const modalCloseBtn = document.querySelector('.form-modal-form span');
+const modalOpenBtn = document.querySelector('.open-modal-btn');
 
 // form elements
 const nameInput = document.querySelector('#name');
@@ -27,6 +38,19 @@ const surnameInput = document.querySelector('#surname');
 const telInput = document.querySelector('#tel');
 const emailInput = document.querySelector('#email');
 const photoLinkInput = document.querySelector('#photo-link');
+
+// create delete btn in card
+const createDeleteBtn = (person) => {
+  const deleteBtn = document.createElement('span');
+  deleteBtn.classList.add('delete-btn');
+  deleteBtn.innerHTML = '<i class="fa-solid fa-user-xmark"></i>';
+  deleteBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    removePersonCard(person);
+    renderPersonsArray();
+  });
+  return deleteBtn;
+}
 
 //create new person card
 const createNewPersonCard = (obj) => {
@@ -49,7 +73,7 @@ const createNewPersonCard = (obj) => {
   const personEmail = document.createElement('a');
   personEmail.classList.add('person-email');
   personEmail.textContent = obj.email;
-  personEmail.href = 'mailto:' + obj.email;
+  personEmail.href = 'mailto:' + obj.email;  // ???
   const personImage = document.createElement('a');
   personImage.classList.add('person-image');
   personImage.textContent = 'Photo link';
@@ -61,28 +85,57 @@ const createNewPersonCard = (obj) => {
   personItem.appendChild(personAvatar);
   personItem.appendChild(personName);
   personItem.appendChild(personLinksWrapper);
+  personItem.appendChild(createDeleteBtn(obj));
 
   personsList.prepend(personItem);
 };
 
-const renderPersonsArray = personsArray.forEach(el => {
-  createNewPersonCard(el);
-});
+const renderPersonsArray = () => {
+  personsList.innerHTML = '';
+  personsArray.forEach((person) => {
+    createNewPersonCard(person);
+  });
+  personsList.appendChild(modalOpenBtn);
+};
 
 // create new Person from form
 const createNewPerson = () => {
-  const newPerson = [];
-  newPerson.name = nameInput.value;
-  newPerson.surname = surnameInput.value;
-  newPerson.tel = telInput.value;
-  newPerson.email = emailInput.value;
-  newPerson.photoLink = photoLinkInput.value;
-
+  const newPerson = {
+    id: Date.now(),
+    name: nameInput.value,
+    surname: surnameInput.value,
+    tel: telInput.value,
+    email: emailInput.value,
+    photoLink: photoLinkInput.valu,
+  };
   personsArray.push(newPerson);
-  createNewPersonCard(newPerson);
-  //renderPersonsArray();
-}
+};
 
+// remove Person 
+const removePersonCard = (person) => {
+  // const index = personsArray.indexOf(person);
+  // if(index > -1){
+  //   personsArray.splice(index, 1);
+  // }
+  personsArray = personsArray.filter(item => item.id !== person.id);
+};
+
+const clickHandler = () => {
+  createNewPerson();
+  renderPersonsArray();
+};
+
+const cleanForm = () => {
+  nameInput.value = '';
+  surnameInput.value = '';
+  telInput.value = '';
+  emailInput.value = '';
+  photoLinkInput.value = '';
+};
+
+const isFormNotValide = () => {
+  return nameInput.value.trim() === '' || telInput.value.trim() === '';
+}
 
 // open modal
 openModalEl.addEventListener('click', () => {
@@ -97,15 +150,14 @@ modalCloseBtn.addEventListener('click', () => {
 // form submit
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  createNewPerson();
+  if(isFormNotValide()){
+    cleanForm();
+    modal.style.display = 'none';
+    return;
+  }
+  clickHandler();
   cleanForm();
   modal.style.display = 'none';
 });
 
-const cleanForm = () => {
-  nameInput.value = '';
-  surnameInput.value = '';
-  telInput.value = '';
-  emailInput.value = '';
-  photoLinkInput.value = '';
-}
+renderPersonsArray(personsArray);
