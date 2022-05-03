0654e3cbbebbd0ffe6e4d5db4b0f328e7c574884
Alexandra <mwalexandra@users.github.com>
2022-05-03 12:28:45 +0200

    tests

--- JavaBasic/Algorithms/22.05.02_points_greedy_alg/src/PointFinderTest.java
+++ JavaBasic/Algorithms/22.05.02_points_greedy_alg/src/PointFinderTest.java
@@ -12,9 +12,9 @@ public class PointFinderTest {
 
   @Test
   void testGetPointsNumber() {
-    cuts.add(new Cut(26, 46));
-    cuts.add(new Cut(38, 40));
-    cuts.add(new Cut(80, 84));
+    cuts.add(new Cut(1, 5));
+    cuts.add(new Cut(3, 8));
+    cuts.add(new Cut(6, 7));
 
     assertEquals(2, test.getPointsNumber(cuts));
   }
@@ -28,4 +28,42 @@ public class PointFinderTest {
     assertEquals(1, test.getPointsNumber(cuts));
   }
 
+  @Test
+  void testGetPointsNumber_1() {
+    cuts.add(new Cut(0, 5));
+    cuts.add(new Cut(1, 2));
+    cuts.add(new Cut(3, 4));
+
+    assertEquals(2, test.getPointsNumber(cuts));
+  }
+
+  @Test
+  void testGetPointsNumber_2() {
+    cuts.add(new Cut(0, 5));
+    cuts.add(new Cut(3, 7));
+    cuts.add(new Cut(6, 8));
+
+    assertEquals(2, test.getPointsNumber(cuts));
+  }
+
+  @Test
+  void testGetPointsNumber_3() {
+    cuts.add(new Cut(5, 8));
+    cuts.add(new Cut(4, 7));
+    cuts.add(new Cut(1, 5));
+    cuts.add(new Cut(2, 3));
+
+    assertEquals(2, test.getPointsNumber(cuts));
+  }
+
+  @Test
+  void testGetPointsNumber_4() {
+    cuts.add(new Cut(2, 4));
+    cuts.add(new Cut(1, 6));
+    cuts.add(new Cut(7, 8));
+    cuts.add(new Cut(3, 9));
+    cuts.add(new Cut(5, 10));
+
+    assertEquals(2, test.getPointsNumber(cuts));
+  }
 }
