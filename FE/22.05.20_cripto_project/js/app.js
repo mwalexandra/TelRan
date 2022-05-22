2b4f4a7ad723e052c9a79555166da824bb4d9193
Alexandra <mwalexandra@users.github.com>
2022-05-22 09:35:08 +0200

    js algorithm description

--- FE/22.05.20_cripto_project/js/app.js
+++ FE/22.05.20_cripto_project/js/app.js
@@ -236,3 +236,29 @@ function recieve(giveValue, giveCurrency, receiveCurrency){
   return receiveValue;
 }
 
+/* 
+1. Берем value из giveInput и selected валюта1 и валюта2 из CurrencySelect
+2. Достаем из объекта result.conversion_rates стоимость валюты1 и валюты2
+3. Считаем: value * курс валюты1 / курс валюты2
+4. Выводим в receiveInput результат
+
+5. Рассчет суммы для 8 других валют (секция Treasury)
+  5.1 Достаем из объекта result.conversion_rates в массив стоимость валюты1, валюты2, валюты3, валюты4, валюты5, валюты6, валюты7, валюты8
+  5.2 Считаем: value * курс валюты1 / курс валюты2
+6. Берем все элементы <li class="treasury__list_item"> </li>
+7. Для каждого элемента массива (forEach) достаем .treasury__receive_currency, .treasury__sum, .treasury__give_currency и складываем туда сответственно 
+    валюту из CurrencySelect (пункт 1), результат рассчета (5.2) и валюту1, 2, 3...
+
+8. Секция Latest applications. Создаем новый объект <li class="applications__list_item"></li>
+  8.1 Кладем внутрь HTML             
+            <p class="applications_list_item_time">ДАТА ОПЕРАЦИИ</p>
+            <p class="applications__give_currency"> VALUE из giveInput
+              <span>ИМЯ валюты из giveCurrencySelect</span>
+            </p>
+            <span><img src="./img/Icons.svg" alt=""></span>
+            <p class="applications__received_currency">VALUE из recieveInput
+              <span>ИМЯ валюты из recieveCurrencySelect</span>
+            </p>
+  8.2 Prepend в applications__list
+  8.3 Пятый элемент листа удаляем                            ---- ???
+*/
