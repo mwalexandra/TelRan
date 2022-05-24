8521ddae56e98fb042533e4c19411240794f4f6a
Alexandra <mwalexandra@users.github.com>
2022-05-24 10:30:55 +0200

    json transform

--- JavaBasic/Algorithms/22.05.17_exersices/src/App.java
+++ JavaBasic/Algorithms/22.05.17_exersices/src/App.java
@@ -1,8 +1,10 @@
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.TreeMap;
+import java.util.Map.Entry;
 
 public class App {
     public static void main(String[] args) {
@@ -99,5 +101,37 @@ public class App {
             leftSum += currentNum;
         }
         return pivotIndex;
+    }
+
+    // Есть Map<String, Object>, олицетворяющий json.
+    // То есть, значениями может быть либо число, либо строка, либо булеан, либо
+    // другой Map<String, Object>.
+    // Известно, что ключи оформлены в виде snake case, то есть "first_key".
+    // Необходимо переделать этот мап таким образом, чтобы ключи стали camelCase.
+    // Подсказка:
+    // надо проверять велью, являются ли они Map, в этом случае рекурсивно вызвать
+    // функцию для него
+
+    public void keysToCamelCase(Map<String, Object> jsonMap) {
+        Set<String> keys = jsonMap.keySet();
+
+        for (String string : keys) {
+
+            // if (jsonMap.get(string) instanceof Map) { проверка вложенности
+            // Map<String, Object> map = jsonMap.get(string);
+            // keysToCamelCase(map);
+            // }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < string.length(); i++) {
+                if (string.charAt(i) == '_') {
+                    char upperCase = string.charAt(++i);
+                    Character.toUpperCase(upperCase);
+                    sb = sb.append(upperCase);
+                } else
+                    sb = sb.append(string.charAt(i));
+            }
+            string = sb.toString();
+        }
     }
 }
